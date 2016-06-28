0d1dc018b4b54adb28498824adf04ef360b76a4c
Alex Gillies <ColossusMini@Alexs-Air-2.lan>
2016-06-28 19:32:22 +0100

    set up the include file references

---
+++ BOMBE/main.cpp
@@ -0,0 +1,13 @@
+/*
+Main enigma file
+used to coordinate all others
+used to encode and decode cyphertext
+*/
+
+#include "reflector.cpp"
+#include "plugboard.cpp"
+#include "rotor_one.cpp"
+#include "rotor_two.cpp"
+#include "rotor_three.cpp"
+#include "rotor_four.cpp"
+#include "rotor_five.cpp"
