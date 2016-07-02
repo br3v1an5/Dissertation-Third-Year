78d55eced231be1a0169d6c9ff821a6294a5ea87
Alex Gillies <ColossusMini@Alexs-Air-2.lan>
2016-07-02 10:28:06 +0100

    testing some cpp stuff and getting the hang of include

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -2,4 +2,31 @@
 Main enigma file
 used to coordinate all others
 used to encode and decode cyphertext
+
+to start:
+
+g++ main.cpp -o main
+./main
 */
+
+#include "plugboard.cpp"
+#include "reflector.cpp"
+#include "rotor_one.cpp"
+#include "rotor_two.cpp"
+#include "rotor_three.cpp"
+#include "rotor_four.cpp"
+#include "rotor_five.cpp"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+
+    setup();
+
+    int value = add_one(5);
+
+    using namespace std;
+    cout << "The value back is " << value << endl;
+}
--- Enigma/plugboard.cpp
+++ Enigma/plugboard.cpp
@@ -5,3 +5,20 @@ map from input to output
 based on the input settings
 */
 
+#include <iostream>
+
+void setup(){
+	std::cout << "this is in setup" << std::endl;
+}
+
+int add_one(int in){
+	return in + 1;
+}
+
+int plugboard_forward(){
+	return 1;
+}
+
+int plugboard_back(){
+	return 1;
+}
