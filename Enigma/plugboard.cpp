32b27c629c2f33c6b567e2e0dbe2ffe8240a5eba
Alex Gillies <ColossusMini@Alexs-MacBook-Air-2.local>
2016-07-11 14:34:40 +0100

    created but not tested the plugboard file

--- Enigma/plugboard.cpp
+++ Enigma/plugboard.cpp
@@ -7,18 +7,53 @@ based on the input settings
 
 #include <iostream>
 
-void setup(){
-	std::cout << "this is in setup" << std::endl;
+int mapping_forward[26];
+int mapping_back[26];
+
+void setup(int connections[26]){
+	/*
+	takes input from the main file
+	in the form of an array that contains all the different
+	connections and a 0 where the connection is unchanged
+	*/
+
+	int counter_0 = 0;
+	// the counter to keep track of the index
+
+	for (int x : connections) {
+		// interate through the input list
+
+		if (x == 0){
+			// if the connection is not specified
+
+			mapping_forward[counter_0] = counter_0;
+			// it is set to itself in the forward mapping
+
+			mapping_back[counter_0] = counter_0;
+			// it is set to itself in the backward mapping
+		}
+		else{
+			// if the connection is specified
+
+			mapping_forward[counter_0] = x;
+			// the forward connection is set
+
+			mapping_back[x] = counter_0;
+			// the backward connection is set
+		}
+		counter_0++;
+		// the index is changed
+	}
 }
 
-int add_one(int in){
-	return in + 1;
+int plugboard_forward(int input){
+	// the forward mapping
+	return mapping_forward[input];
+	// returns the letter that the input is mapped to
 }
 
-int plugboard_forward(){
-	return 1;
-}
-
-int plugboard_back(){
-	return 1;
+int plugboard_back(int input){
+	// the backward mapping
+	return mapping_back[input];
+	// returns the letter that was mapped to the input
 }
