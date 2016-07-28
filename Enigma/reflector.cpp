b4fa8e985f365466dedf9ed6d4e8d125fa52fd3e
Alex Gillies <ColossusMini@Alexs-MacBook-Air-2.local>
2016-07-28 15:27:54 +0100

    made the reflector file. yet to test

--- Enigma/reflector.cpp
+++ Enigma/reflector.cpp
@@ -5,3 +5,34 @@ map from input to output
 based on the input settings
 */
 
+int reflector[26];
+
+
+void setup(int connections[26]){
+	/*
+	takes input from the main file
+	in the form of an array that contains all the different
+	connections and a 0 where the connection is unchanged
+	*/
+
+	counter_0 = 0;
+	// set the counter to zero
+
+	for (int x : connections) {
+		// interate through the input list
+
+		reflector[counter_0] = x;
+		// the connections are made
+	
+		counter_0++;
+		// increment the counter
+	}
+}
+
+int get_reflection(int index){
+	/*
+	take the input from the main file and return the corresponding letter
+	*/
+
+	return reflector[index];
+}
